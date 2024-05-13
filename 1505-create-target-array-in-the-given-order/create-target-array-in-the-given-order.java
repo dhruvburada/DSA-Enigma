2ed1f6f3747b658ddadc69cff83fc1e8e981c44e
Dhruv Burada <121245063+dhruvburada@users.noreply.github.com>
2024-05-13 20:06:10 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (27.76%) - LeetSync

--- 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.java
+++ 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.java
@@ -1,26 +1,15 @@
-import java.util.*;
-
 class Solution {
+    public int[] createTargetArray(final int[] nums, final int[] index) {
+        final int n = nums.length;
+        final int[] result = new int[n];
+        final List<Integer> list = new ArrayList<>();
 
-    public int[] createTargetArray(int[] nums, int[] index) {
-        int target[] = new int[nums.length]; // Create target array with the same length as nums
-        Arrays.fill(target, -1111); // Fill target array with a placeholder value
+        for(int i = 0; i < n; ++i)
+            list.add(index[i], nums[i]);
 
-        for (int i = 0; i < target.length; i++) {
-            int currentIndex = index[i]; // Get the current index from the index array
+        for(int i = 0; i < n; ++i)
+            result[i] = list.get(i);
 
-            if (target[currentIndex] != -1111) { // Check if the target at currentIndex is not empty
-                int j = target.length - 1; // Start shifting elements from the end of the array
-
-                while (j != currentIndex) {
-                    target[j] = target[j - 1]; // Shift elements to the right
-                    j--; // Decrement j
-                }
-            }
-
-            target[currentIndex] = nums[i]; // Set the value at currentIndex to nums[i]
-        }
-
-        return target;
+        return result;
     }
 }
