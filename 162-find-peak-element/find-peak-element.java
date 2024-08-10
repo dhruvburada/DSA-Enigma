908f21f86cb07653718df92462eec0fd97455abd
Dhruv Burada <121245063+dhruvburada@users.noreply.github.com>
2024-08-10 11:43:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (35.89%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,18 +1,24 @@
 class Solution {
     public int findPeakElement(int[] nums) {
+
+        //since we are checking for first and last element seperately and we dont want mid=-1 or mid=n thats why we use low and high:
         int low=1;
-        int last_index = nums.length-1;
         int high=nums.length-2;
+        int last_index = nums.length-1;
         int mid;
 
+        //If an array has only one element that means it is peak element itself
         if(nums.length==1)
         {
             return 0;
         }
+        //If the first element is greater than element in right that means its peak element we dont check in right
         else if (nums[0]>nums[1])
         {
            return 0;
         }
+
+//If the right most element is greater than an element before then its peak element we dont check in right
         else if(nums[last_index]>nums[last_index-1])
         {
             return last_index;
